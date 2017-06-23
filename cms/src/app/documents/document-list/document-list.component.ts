81bdabac0309205722adf44b97548a7b8c46136b
Hansen Muhile <hanscheng94@gmail.com>
2017-06-23 05:16:29 -0600

    Lesson 4

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Document} from '../document.model'
 
 @Component({
   selector: 'cms-document-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+
+
+  documents: Document[] = [
+    new Document(1,"Homework","this is the work you do at home","homework.com"),
+    new Document(2,"Classwork","this is the work you do in class","classwork.com"),
+    new Document(3,"Gymwork","this is the work you do at the gym","gymwork.com")
+  ];
+
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  onSelectedDocument(document:Document){
+    this.selectedDocumentEvent.emit(document);
+  };
 
   constructor() { }
 
--- cms/src/app/documents/document.model.ts
+++ cms/src/app/documents/document.model.ts
@@ -3,15 +3,31 @@ export class Document {
   public name: string;
   public description: string;
   public url: string;
-  public children: any;
 
-  constructor(documentID: number, name: string, description: string, url: string, children:any){
+  constructor(documentID: number, name: string, description: string, url: string){
     this.documentId = documentID;
     this.name = name;
     this.description = description;
     this.url = url;
-    this.children = children;
 
   }
 
 }
+
+// export class Document {
+//   public documentId: number;
+//   public name: string;
+//   public description: string;
+//   public url: string;
+//   // public children: any;
+//
+//   constructor(documentID: number, name: string, description: string, url: string, children:any){
+//     this.documentId = documentID;
+//     this.name = name;
+//     this.description = description;
+//     this.url = url;
+//     this.children = children;
+//
+//   }
+//
+// }
